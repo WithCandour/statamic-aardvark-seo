e9d39b55f62ba195a28e07abe32a122aee0e61bc
Andrew Haine <andrew@andrewhaine.co.uk>
2019-04-15 11:14:13 +0100

    Remove unnecessary xhtml namespace declaration

--- AardvarkSeo/resources/views/sitemap_index.blade.php
+++ AardvarkSeo/resources/views/sitemap_index.blade.php
@@ -1,5 +1,5 @@
 {!! $xmlDefinition !!}{!! $xslLink !!}
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach( $sitemaps as $sitemap )
   @if( count($sitemap->getSitemapItems()) >= 1 )
     <sitemap>
