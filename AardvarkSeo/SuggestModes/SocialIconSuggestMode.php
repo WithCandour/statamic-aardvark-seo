a50e0700eb1b5ad66fcd19007766eb62fb1c1073
Andrew Haine <andrew@andrewhaine.co.uk>
2020-05-05 10:50:19 +0100

    Add the ability to define your own social types

--- AardvarkSeo/SuggestModes/SocialIconSuggestMode.php
+++ AardvarkSeo/SuggestModes/SocialIconSuggestMode.php
@@ -6,6 +6,38 @@ use Statamic\Addons\Suggest\Modes\AbstractMode;
 
 class SocialIconSuggestMode extends AbstractMode
 {
+
+    public $default_socials = [
+        ['value' => 'amazon', 'text' => 'Amazon'],
+        ['value' => 'android', 'text' => 'Android'],
+        ['value' => 'apple', 'text' => 'Apple'],
+        ['value' => 'behance', 'text' => 'Behance'],
+        ['value' => 'bitbucket', 'text' => 'Bitbucket'],
+        ['value' => 'codepen', 'text' => 'Codepen'],
+        ['value' => 'dribbble', 'text' => 'Dribbble'],
+        ['value' => 'facebook', 'text' => 'Facebook'],
+        ['value' => 'flickr', 'text' => 'Flickr'],
+        ['value' => 'foursquare', 'text' => 'Foursquare'],
+        ['value' => 'github', 'text' => 'Github'],
+        ['value' => 'google-plus', 'text' => 'Google Plus'],
+        ['value' => 'instagram', 'text' => 'Instagram'],
+        ['value' => 'linkedin', 'text' => 'LinkedIn'],
+        ['value' => 'medium', 'text' => 'Medium'],
+        ['value' => 'meetup', 'text' => 'Meetup'],
+        ['value' => 'pinterest', 'text' => 'Pinterest'],
+        ['value' => 'reddit', 'text' => 'Reddit'],
+        ['value' => 'skype', 'text' => 'Skype'],
+        ['value' => 'slack', 'text' => 'Slack'],
+        ['value' => 'soundcloud', 'text' => 'Soundcloud'],
+        ['value' => 'spotify', 'text' => 'Spotify'],
+        ['value' => 'twitch', 'text' => 'Twitch'],
+        ['value' => 'twitter', 'text' => 'Twitter'],
+        ['value' => 'tumblr', 'text' => 'Tumblr'],
+        ['value' => 'whatsapp', 'text' => 'WhatsApp'],
+        ['value' => 'yelp', 'text' => 'Yelp'],
+        ['value' => 'youtube', 'text' => 'YouTube'],
+    ];
+
     /**
      * Return a list of social icons.
      *
@@ -13,35 +45,15 @@ class SocialIconSuggestMode extends AbstractMode
      */
     public function suggestions()
     {
-        return [
-            ['value' => 'amazon', 'text' => 'Amazon'],
-            ['value' => 'android', 'text' => 'Android'],
-            ['value' => 'apple', 'text' => 'Apple'],
-            ['value' => 'behance', 'text' => 'Behance'],
-            ['value' => 'bitbucket', 'text' => 'Bitbucket'],
-            ['value' => 'codepen', 'text' => 'Codepen'],
-            ['value' => 'dribbble', 'text' => 'Dribbble'],
-            ['value' => 'facebook', 'text' => 'Facebook'],
-            ['value' => 'flickr', 'text' => 'Flickr'],
-            ['value' => 'foursquare', 'text' => 'Foursquare'],
-            ['value' => 'github', 'text' => 'Github'],
-            ['value' => 'google-plus', 'text' => 'Google Plus'],
-            ['value' => 'instagram', 'text' => 'Instagram'],
-            ['value' => 'linkedin', 'text' => 'LinkedIn'],
-            ['value' => 'medium', 'text' => 'Medium'],
-            ['value' => 'meetup', 'text' => 'Meetup'],
-            ['value' => 'pinterest', 'text' => 'Pinterest'],
-            ['value' => 'reddit', 'text' => 'Reddit'],
-            ['value' => 'skype', 'text' => 'Skype'],
-            ['value' => 'slack', 'text' => 'Slack'],
-            ['value' => 'soundcloud', 'text' => 'Soundcloud'],
-            ['value' => 'spotify', 'text' => 'Spotify'],
-            ['value' => 'twitch', 'text' => 'Twitch'],
-            ['value' => 'twitter', 'text' => 'Twitter'],
-            ['value' => 'tumblr', 'text' => 'Tumblr'],
-            ['value' => 'whatsapp', 'text' => 'WhatsApp'],
-            ['value' => 'yelp', 'text' => 'Yelp'],
-            ['value' => 'youtube', 'text' => 'YouTube'],
-        ];
+
+        $custom_socials = collect($this->getConfig('custom_socials'))->map(function ($social) {
+            return [
+                'value' => $social['social'],
+                'text' => $social['label']
+            ];
+        });
+
+        return collect($this->default_socials)->merge($custom_socials)->sortBy('text')->values();
+
     }
 }
