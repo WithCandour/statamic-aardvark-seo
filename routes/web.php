57f9eedc12ec59b066a5e313fb7199640c037270
Andrew Haine <andrew@andrewhaine.co.uk>
2020-11-30 14:49:01 +0000

    Add names to sitemap routes to fix artisan:optimize command

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use Statamic\Facades\URL;
 Route::namespace('\WithCandour\AardvarkSeo\Http\Controllers\Web')
     ->name('aardvark-xml-sitemap')
     ->group(function () {
-        Route::get('sitemap.xml', 'SitemapController@index');
-        Route::get('sitemap_{handle}.xml', 'SitemapController@single');
-        Route::get('aardvark-sitemap.xsl', 'SitemapController@xsl');
+        Route::get('sitemap.xml', 'SitemapController@index')
+            ->name('index');
+        Route::get('sitemap_{handle}.xml', 'SitemapController@single')
+            ->name('single');
+        Route::get('aardvark-sitemap.xsl', 'SitemapController@xsl')
+            ->name('xsl');
 
         // Add sitemap routes for non-domain-level multisites
         $roots = Site::all()->map(function ($site) {
@@ -18,8 +21,11 @@ Route::namespace('\WithCandour\AardvarkSeo\Http\Controllers\Web')
         })->unique();
 
         $roots->each(function ($root) {
-            Route::get("${root}/sitemap.xml", 'SitemapController@index');
-            Route::get("${root}/sitemap_{handle}.xml", 'SitemapController@single');
-            Route::get("${root}/aardvark-sitemap.xsl", 'SitemapController@xsl');
+            Route::get("${root}/sitemap.xml", 'SitemapController@index')
+                ->name("${root}.index");
+            Route::get("${root}/sitemap_{handle}.xml", 'SitemapController@single')
+                ->name("${root}.single");
+            Route::get("${root}/aardvark-sitemap.xsl", 'SitemapController@xsl')
+                ->name("${root}.xsl");
         });
     });
