b4f8f3fddb99d06fce29786dcb4974461e93e4c3
Andrew Haine <andrew@andrewhaine.co.uk>
2021-01-25 08:27:42 +0000

    Update laravel mix version and other flagged dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js/aardvark-seo.js');
+mix.js('resources/js/app.js', 'public/js/aardvark-seo.js').vue({ version: 2 });
 mix.styles('resources/css/app.css', 'public/css/aardvark-seo.css');
