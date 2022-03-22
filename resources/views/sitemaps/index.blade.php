7e35272e349a866f024490b893c94f68a5950d95
Andrew Haine <andrew@andrewhaine.co.uk>
2022-03-22 12:29:55 +0000

    Remove custom aardvark version attribute from sitemaps

--- resources/views/sitemaps/index.blade.php
+++ resources/views/sitemaps/index.blade.php
@@ -1,5 +1,5 @@
 {!! $xmlDefinition !!}{!! $xslLink !!}
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" aardvark-version="{{ $version }}">
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach( $sitemaps as $sitemap )
   @if( count($sitemap->getSitemapItems()) >= 1 )
     <sitemap>
