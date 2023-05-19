2e7678b10710518dd8196324af6f33d52c068563
Joshua Noyes <joshua.noyes@withcandour.co.uk>
2023-05-19 16:35:53 +0100

    Use class reference syntax for routes

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,15 @@
 
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
+use WithCandour\AardvarkSeo\Http\Controllers\Web\SitemapController;
 
-Route::namespace('\WithCandour\AardvarkSeo\Http\Controllers\Web')
-    ->name('aardvark-xml-sitemap')
+Route::name('aardvark-xml-sitemap')
     ->group(function () {
-        Route::get('sitemap.xml', 'SitemapController@index')
+        Route::get('sitemap.xml', [SitemapController::class, 'index'])
             ->name('index');
-        Route::get('sitemap_{handle}.xml', 'SitemapController@single')
+        Route::get('sitemap_{handle}.xml', [SitemapController::class, 'single'])
             ->name('single');
-        Route::get('aardvark-sitemap.xsl', 'SitemapController@xsl')
+        Route::get('aardvark-sitemap.xsl', [SitemapController::class, 'xsl'])
             ->name('xsl');
 
         // Add sitemap routes for non-domain-level multisites
@@ -21,11 +21,11 @@ Route::namespace('\WithCandour\AardvarkSeo\Http\Controllers\Web')
         })->unique();
 
         $roots->each(function ($root) {
-            Route::get("${root}/sitemap.xml", 'SitemapController@index')
-                ->name("${root}.index");
-            Route::get("${root}/sitemap_{handle}.xml", 'SitemapController@single')
-                ->name("${root}.single");
-            Route::get("${root}/aardvark-sitemap.xsl", 'SitemapController@xsl')
-                ->name("${root}.xsl");
+            Route::get("{$root}/sitemap.xml", [SitemapController::class, 'index'])
+                ->name("{$root}.index");
+            Route::get("{$root}/sitemap_{handle}.xml", [SitemapController::class, 'single'])
+                ->name("{$root}.single");
+            Route::get("{$root}/aardvark-sitemap.xsl", [SitemapController::class, 'xsl'])
+                ->name("{$root}.xsl");
         });
     });
